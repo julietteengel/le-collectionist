380b378d3c150f8380063af87cf33096ac06b403
Juliette Engel <juliette.engel@skema.edu>
2022-11-03 13:20:10 +0100

    add view and request tests

---
+++ spec/requests/shops_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Shops", type: :request do
+
+  let(:shop_with_hours) { create(:shop_with_hours) }
+
+  describe "GET /show" do
+    it "renders a successul response" do
+      get "/shops/#{shop_with_hours.id}"
+      expect(response).to be_successful
+    end
+  end
+
+end
---
+++ spec/views/shops/show.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "shops/show", type: :view do
+
+  let(:shop_with_hours) { create(:shop_with_hours) }
+
+  before do
+    assign(:shop, shop_with_hours)
+  end
+
+  it "renders a list of opening hours for each day of the week" do
+    render
+    expect(rendered).to have_text "#{shop_with_hours.name}"
+    # A line for each day of the week
+    expect(rendered).to have_selector("tr", count: 7)
+    # First line should be today
+    expect(rendered).to have_selector("td", class: "today", text: I18n.t("activerecord.attributes.opening_hour.days.#{Date.today.strftime("%A").downcase}"))
+
+  end
+end
