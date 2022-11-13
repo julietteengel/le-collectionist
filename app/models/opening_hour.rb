2b63e89a2bc6bf97776f2b4cbca32cc70de27c19
Juliette Engel <juliette.engel@skema.edu>
2022-11-13 10:34:43 +0100

    add overlapping validation and test model

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -2,6 +2,7 @@ class OpeningHour < ApplicationRecord
   belongs_to :shop
   validates_presence_of :day, :closes_at, :opens_at
   validate :opens_before_closes
+  validate :not_overlapping_another_opening_hour?
 
   enum day: {
     sunday: 0,
@@ -13,9 +14,25 @@ class OpeningHour < ApplicationRecord
     saturday: 6
   }
 
+  scope :for_shop, -> (shop) { where(shop: shop) }
+  scope :for_day, -> (day) { where(day: day) }
+
+  scope :overlapping, -> (start_time, end_time) { where("opens_at <= ? AND ? <= closes_at", end_time, start_time) }
+
   private
 
   def opens_before_closes
     errors.add(:base, I18n.t(:opens_before_closes, scope: 'activerecord.errors.models.shop')) if opens_at && closes_at && opens_at >= closes_at
+  end
+
+  def overlapping_opening_hours
+    OpeningHour.for_shop(shop).for_day(day).overlapping(opens_at, closes_at)
+  end
+
+  def not_overlapping_another_opening_hour?
+    return true if overlapping_opening_hours.empty? # early-return if no overlap
+    
+    errors.add(:base, I18n.t(:has_overlapping_opening_hours, scope: 'activerecord.errors.models.shop'))
+    false
   end
 end
--- spec/factories/opening_hours.rb
+++ spec/factories/opening_hours.rb
@@ -1,10 +1,9 @@
 FactoryBot.define do
   factory :opening_hour do
-
     # relations
     association :shop
 
-    day { [:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday].sample }
+    day { %i[sunday monday tuesday wednesday thursday friday saturday].sample }
 
     factory :default_opening_hour do
       opens_before_closes
@@ -12,6 +11,10 @@ FactoryBot.define do
 
     factory :opening_hour_closes_before_opens do
       closes_before_opens
+    end
+
+    factory :opening_hour_overlapping_opening_hours do
+      overlapping
     end
 
     trait :opens_before_closes do
@@ -34,5 +37,13 @@ FactoryBot.define do
       closes_at { rand(t1..t2) }
     end
 
+    trait :overlapping do
+      t1 = Faker::Time.between(from: Time.zone.parse('00:00'), to: Time.zone.parse('15:00'))
+      max = Time.zone.parse('10:30') > t1 ? Time.zone.parse('10:30') : t1
+      t2 = Faker::Time.between(from: max, to: Time.zone.parse('23:59'))
+      # where('opens_at <= ? AND ? <= closes_at', end_time, start_time)
+      opens_at { t1 }
+      closes_at { t2 }
+    end
   end
 end
