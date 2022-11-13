4235c1ef2ead22f9b07b3671487a0ed6d9a44e92
Juliette Engel <juliette.engel@skema.edu>
2022-11-13 21:48:56 +0100

    rubocop style improvements

--- spec/views/shops/index.html.erb_spec.rb
+++ spec/views/shops/index.html.erb_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'shops/index', type: :view do
     assign(:shop, shop_with_hours)
     assign(:shops, Shop.all)
   end
-  
+
   it 'renders a list of all shops' do
     render
     # A line for each shop
